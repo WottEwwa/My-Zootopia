090ad45e36bbfc49b7111a17b97ce57cdcd5a959
Patrick <p.lengties@posteo.de>
2025-03-27 17:14:40 +0100

    bonus: add all existing jason keys

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,18 +9,59 @@ def load_data(file_path):
 # Load animals data
 animals_data = load_data('animals_data.json')
 
-# Generate animals information as plain text
-output = ''
-for animal in animals_data:
-    output += f'<li class="cards__item">\n'
-    output += f'<div class="card__title">{animal.get('name', 'N/A')}</div>\n'
-    output += f'<p class="card__text">\n'
-    output += f'<strong>Diet:</strong> {animal.get('characteristics', {}).get('diet', 'N/A')}<br/>\n'
-    output += f'<strong>Locations:</strong> {', '.join(animal.get('locations', ['N/A']))}<br/>\n'
-    output += f'<strong>Type:</strong> {animal.get('characteristics', {}).get('type', 'N/A')}<br>\n'
-    output += f'</p></li>\n'
+def serialize_animal(animal_obj):
+    """Serialize an animal object to HTML"""
+    output = ''
 
-    print(output)
+    output += '<li class="cards__item">\n'
+    output += f'<div class="card__title">{animal_obj.get("name", "N/A")}</div>\n'
+
+    output += '<div class="card__text">\n'
+    output += '<ul>\n'
+    output += f'<li><strong>Diet:</strong> {animal_obj.get("characteristics", {}).get("diet", "N/A")}</li>\n'
+    output += f'<li><strong>Locations:</strong> {", ".join(animal_obj.get("locations", ["N/A"]))}</li>\n'
+    output += f'<li><strong>Type:</strong> {animal_obj.get("characteristics", {}).get("type", "N/A")}</li>\n'
+    output += '</ul>\n'
+    output += '</div>\n'
+
+    # Characteristic details
+    characteristic = animal_obj.get("characteristics", {})
+    if characteristic:
+        output += '<details>\n'
+        output += '<summary>Additional characteristics</summary>\n'
+        output += '<ul>\n'
+        
+        for key, value in characteristic.items():
+            if key not in ["diet", "type"]:
+                output += f'<li><strong>{key.replace("_", " ").title()}:</strong> {value}</li>\n'
+        
+        output += '</ul>\n'
+        output += '</details>\n'
+
+    # Taxonomy details
+    taxonomy = animal_obj.get("taxonomy", {})
+    if taxonomy:
+        output += '<details>\n'
+        output += '<summary>Taxonomic classification</summary>\n'
+        output += '<ul>\n'
+        output += '<li><strong>Kingdom:</strong> {}</li>\n'.format(taxonomy.get("kingdom", "N/A"))
+        output += '<li><strong>Phylum:</strong> {}</li>\n'.format(taxonomy.get("phylum", "N/A"))
+        output += '<li><strong>Class:</strong> {}</li>\n'.format(taxonomy.get("class", "N/A"))
+        output += '<li><strong>Order:</strong> {}</li>\n'.format(taxonomy.get("order", "N/A"))
+        output += '<li><strong>Family:</strong> {}</li>\n'.format(taxonomy.get("family", "N/A"))
+        output += '<li><strong>Genus:</strong> {}</li>\n'.format(taxonomy.get("genus", "N/A"))
+        output += '<li><strong>Scientific Name:</strong> {}</li>\n'.format(taxonomy.get("scientific_name", "N/A"))
+        output += '</ul>\n'
+        output += '</details>\n'
+
+    output += '</div></li>\n'
+
+    return output
+
+
+output = ''
+for animal_obj in animals_data:
+    output += serialize_animal(animal_obj)
 
 try:
     # Load template
